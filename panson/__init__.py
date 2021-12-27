8476e57b0c8e59345b5c0b6ebbab1dddd2d29701
mnalli <michele.nalli@studio.unibo.it>
2021-12-27 20:07:15 +0100

    Structure of initial design

---
+++ panson/__init__.py
@@ -0,0 +1,14 @@
+"""Framework for the development of interactive sonification applications.
+
+Collection of base classes and functions to help the development
+of interactive sonification applications within python and jupyter
+notebooks.
+"""
+
+
+from .sonification import Sonification
+
+
+__all__ = [
+    'Sonification',
+]
