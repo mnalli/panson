4bcc0d29f84567908dd016c36ed5b82a095fb733
mnalli <michele.nalli@studio.unibo.it>
2022-01-05 18:17:38 +0100

    Add multithreading to DataPlayer
    This is only a prototype.

--- panson/data_players.py
+++ panson/data_players.py
@@ -12,16 +12,27 @@ _LOGGER = logging.getLogger(__name__)
 
 class DataPlayer:
 
-    def __init__(self, sonification=None, data=None):
+    def __init__(self, sonification=None, data=None, fps=None):
         self.sonification = sonification
         if data is None:
             self.df = None
         else:
             self.load(data)
 
+        self._fps = fps
+        # index of the current data point to play
+        self._ptr = 0
+
         # reference to default server
         self._s = scn.SC.get_default().server
         self._recorder = None
+
+        # worker thread
+        self._worker = None
+
+        # running info and lock
+        self._running = False
+        self._running_lock = Lock()
 
     def load(self, data):
         if type(data) == type(str):
@@ -47,7 +58,6 @@ class DataPlayer:
 
         # TODO: is it better to instantiate asap?
         self._s.bundler().add(self.sonification.start()).send()
-        # Bundler().add(son.start(sc.server)).send()
 
         t0 = time()
 
@@ -63,6 +73,79 @@ class DataPlayer:
 
         # send stop bundle
         self._s.bundler().add(self.sonification.stop()).send()
+
+    def play(self, rate=1):
+        with self._running_lock:
+            if self._running:
+                raise ValueError("Already playing!!")
+
+        self._worker = Thread(name='player', target=self._play, args=(rate,))
+        self._worker.start()
+
+    def _play(self, rate):
+        _LOGGER.info('player thread starting')
+
+        with self._running_lock:
+            self._running = True
+
+        # TODO: do it every time???
+        # load synthdefs on the server
+        self._s.bundler().add(self.sonification.initialize()).send()
+
+        # TODO: is it better to instantiate asap?
+        self._s.bundler().add(self.sonification.start()).send()
+
+        # assumpions:
+        #   no fps mode
+        #   timestamp contains time info
+        #   positive rate
+        #   no seek while playing
+
+        start_ptr = self._ptr
+        t0 = time()
+        start_timestamp = self.df.iloc[start_ptr].timestamp
+
+        # iterate over dataframe rows, from the current element on
+        for _, row in self.df.iloc[start_ptr:-1].iterrows():
+            with self._running_lock:
+                if not self._running:
+                    # pause was called
+                    break
+
+            # process, bundle and send
+            target_time = t0 + (row.timestamp - start_timestamp) / rate
+            self._s.bundler(target_time).add(self.sonification.process(row)).send()
+
+            self._ptr += 1
+
+            # sleep for the missing time
+            waiting_time = target_time - time()
+            if waiting_time > 0:
+                sleep(waiting_time)
+
+        # send stop bundle
+        self._s.bundler().add(self.sonification.stop()).send()
+
+        # this is relevant when the for loop ends naturally
+        with self._running_lock:
+            self._running = False
+        _LOGGER.info('player thread exiting')
+
+    def pause(self):
+        with self._running_lock:
+            if not self._running:
+                raise ValueError('Already paused!')
+            # stop workin thread
+            self._running = False
+        self._worker.join()
+
+    def seek(self, idx):
+        if not (0 <= idx <= self.df.index[-1]):
+            raise ValueError(
+                f"Invalid index {idx}."
+                f"Must be in range [0, {self.df.index[-1]}]"
+            )
+        self._ptr = idx
 
     # TODO: the code in RTDataPlayer is duplicated...
     def record_start(self, path='record.wav'):
@@ -118,7 +201,6 @@ class DataPlayer:
 
     # def __repr__(self):
     #     pass
-
 
 
 # TODO: add listening hooks
