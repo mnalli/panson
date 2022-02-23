220c434ba7b980ccc3a7298d8a74bb4a430429a7
mnalli <michele.nalli@studio.unibo.it>
2022-02-23 17:23:59 +0100

    Add dummy data generators
    They will be needed to test multiple streams

--- panson/data_gens.py
+++ panson/data_gens.py
@@ -1,9 +1,9 @@
 
 import pandas as pd
 import csv
+from itertools import count
 import time
-
-from math import sin
+import math
 
 
 def csv_fifo_gen(fifo_path: str):
@@ -29,10 +29,29 @@ def csv_fifo_gen(fifo_path: str):
 def dummy_sin_gen(fps=30, amp=1):
     """Yields sinusoidal values varying with time."""
 
+    header = ['timestamp', 'value']
     t0 = time.time()
 
-    while True:
-        delta = time.time() - t0
-        yield sin(delta) * amp
+    for i in count():
+        t = time.time() - t0
+        value = math.sin(t) * amp
+        # TODO: remove explicit name
+        yield pd.Series([t, value], header, name=i)
+        # TODO: improve timing
+        time.sleep(1 / fps)
+
+
+def dummy_sin_cos_gen(fps=30, sin_amp=1, cos_amp=1):
+    """Yields oscillatory values varying with time."""
+
+    header = ['timestamp', 'sin', 'cos']
+    t0 = time.time()
+
+    for i in count():
+        t = time.time() - t0
+        sin = math.sin(t) * sin_amp
+        cos = math.cos(t) * cos_amp
+        # TODO: remove explicit name
+        yield pd.Series([t, sin, cos], header, name=i)
         # TODO: improve timing
         time.sleep(1 / fps)
